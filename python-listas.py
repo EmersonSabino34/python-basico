e10967742f82595e0f9f398aa5f6239af15349c6
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-08-24 22:02:19 +0100

    Update python-listas.py

--- python-listas.py
+++ python-listas.py
@@ -2,6 +2,7 @@
 # Comentários em Português e prints para facilitar o entendimento.
 # Compatível com Python 3.x — rode: python treinamento_basico_listas.py
 
+
 # -----------------------
 # 1) Listas (mutable)
 # -----------------------
